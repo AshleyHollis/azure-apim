f8052eff94b815cd05b8af7241fc0c09b75a05c1
Ashley Hollis <Ashley.Hollis@LAB3.com.au>
2023-09-16 18:19:11 +1000

    Add storage account for function apps

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -6,12 +6,23 @@ resource "azurerm_service_plan" "main" {
   sku_name            = "Y1"
 }
 
+resource "azurerm_storage_account" "fnapps" {
+  name                     = "ahfnapps"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_linux_function_app" "apiapp01" {
   name                = "ahapiapp01"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
 
-  service_plan_id = azurerm_service_plan.main.id
+  storage_account_name       = azurerm_storage_account.fnapps.name
+  storage_account_access_key = azurerm_storage_account.fnapps.primary_access_key
+
+  service_plan_id = azurerm_service_plan.fnapps.id
 
   site_config {}
 }

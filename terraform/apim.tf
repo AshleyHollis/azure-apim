ece32c166a90ce2c217642f377c4dcaf9aac24bc
Ashley Hollis <Ashley.Hollis@LAB3.com.au>
2023-09-16 16:44:40 +1000

    Initial commit

---
+++ terraform/apim.tf
@@ -0,0 +1,9 @@
+resource "azurerm_api_management" "main" {
+  name                = "ahapim"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  publisher_name      = "Ashley Hollis"
+  publisher_email     = "me@ashleyhollis.com"
+
+  sku_name = "Developer_1"
+}

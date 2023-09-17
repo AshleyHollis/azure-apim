9d923dde26636f042e9432bbbf5e928ae4a6a881
Ashley Hollis <Ashley.Hollis@LAB3.com.au>
2023-09-17 20:19:06 +1000

    Fix resource_id for azurerm_api_management_backend.auth-tester

--- terraform/apim_config/apim_backend.tf
+++ terraform/apim_config/apim_backend.tf
@@ -4,5 +4,16 @@ resource "azurerm_api_management_backend" "auth-tester" {
   api_management_name = local.api_management_name
   protocol            = "http"
   url                 = "https://ahapiapp01.azurewebsites.net"
-  # resource_id         = "/subscriptions/28aefbe7-e2af-4b4a-9ce1-92d6672c31bd/resourceGroups/apim/providers/Microsoft.Web/sites/ahapiapp01"
+  resource_id         = "https://management.azure.com/subscriptions/28aefbe7-e2af-4b4a-9ce1-92d6672c31bd/resourceGroups/apim/providers/Microsoft.Web/sites/ahapiapp01"
+
+  credentials {
+    header = {
+      x-functions-key = "{{ahapiapp01-key}}"
+    }
+  }
+
+  tls {
+    validate_certificate_chain = true
+    validate_certificate_name  = true
+  }
 }
